6d2cfb600ce629913cc219257423c99a4fa3d4b7
Vladimir <vlborshh@yandex.ru>
2022-12-22 12:54:47 +0300

    добавил код с комментариями

--- ItogBlokaC/Program.cs
+++ ItogBlokaC/Program.cs
@@ -1,7 +1,7 @@
 ﻿string[] array = { "hello", "2", "world", ":-", "-2", "1587", "98", "ddwd9", "-ff" };
 string[] mass = { };
 int count = 0;
-
+// печатаем массив
 void PrintArray(string[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
@@ -10,17 +10,18 @@ void PrintArray(string[] arr)
     }
     Console.WriteLine();
 }
-
+//заполняем массив mass 
 for (int i = 0; i < array.Length; i++)
 {
     if (array[i].Length <= 3)
     {
         count++;
-        Array.Resize(ref mass, count);
+        Array.Resize(ref mass, count); // увеличиваем размер массива до заданной величины (по факту на "1")
         mass[count - 1] = array[i];
 
     }
 }
+// передаем массивы на печать
 Console.WriteLine("Исходный массив:");
 PrintArray(array);
 Console.WriteLine("Результат:");
