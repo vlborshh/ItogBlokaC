1d150626d5b2d337b550505c5147d1bba950da51
Vladimir <vlborshh@yandex.ru>
2022-12-22 12:48:04 +0300

    Добавил блок-схему алгоритма

--- ItogBlokaC/Program.cs
+++ ItogBlokaC/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "hello", "2", "world", ":-", "-2", "1587", "98", "ddwd9", "-ff" };
+string[] mass = { };
+int count = 0;
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        count++;
+        Array.Resize(ref mass, count);
+        mass[count - 1] = array[i];
+
+    }
+}
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+Console.WriteLine("Результат:");
+PrintArray(mass);
+
